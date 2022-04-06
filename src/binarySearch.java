05fb4a9f0af8daab0dc552db5b95a3310f03736b
unknown <you@example.com>
2022-04-06 08:13:03 +0700

    first commit

---
+++ src/binarySearch.java
@@ -0,0 +1,23 @@
+public class binarySearch {
+    public static int search(int[] arr, int target){
+        int low = 0;
+        int high = arr.length-1;
+        while (low<=high){
+            int middle = (low+high)/2;
+            if (arr[middle]<target){
+                low = middle+1;
+            }
+            else if (arr[middle]>target){
+                high = middle-1;
+            }
+            else
+                return middle;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+         System.out.println(search(list,45));
+    }
+}
